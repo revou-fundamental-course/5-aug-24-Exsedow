34fbe4028007bd3155619ab0566e310969d70f43
Exsedow <endybjb22@gmail.com>
2024-08-13 02:44:18 +0800

    update ke-3 html dan javaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-// ini js
+function hitungBMI() {
+    const beratBadan = parseFloat(document.getElementById("berat-badan").value);
+    const tinggiBadan = parseFloat(document.getElementById("tinggi-badan").value);
+    const hasil = beratBadan / ((tinggiBadan / 100) * (tinggiBadan / 100));
+    let status;
+
+    if (hasil < 18.5) {
+        status = "Kurang dari 18.5 - Berat badan kurang";
+    } else if (hasil >= 18.5 && hasil <= 24.9) {
+        status = "18.5 - 24.9 - Berat badan normal (ideal)";
+    } else if (hasil >= 25 && hasil <= 29.9) {
+        status = "25 - 29.9 - Sedikit kelebihan berat badan";
+    } else if (hasil >= 30) {
+        status = "Lebih dari 30 - Obesitas";
+    }
+
+    document.getElementById("status").innerHTML = status;
+    document.getElementById("hasil").innerHTML = hasil.toFixed(2);
+}
